42dc66a774b866ba433bd0be00bc1f01cdea26c5
iniken <kiparis994@gmail.com>
2020-08-08 00:04:10 +0700

    10) add default auto import

--- src/Header.js
+++ src/Header.js
@@ -3,9 +3,9 @@ import React from "react";
 const Header = () => {
     return (
         <div>
-            <a href="#S">Home</a>
-            <a href="#s">News Feed</a>
-            <a href="#s">Message</a>
+            <a href="#S">Home</a> -
+            <a href="#s">News Feed</a> -
+            <a href="#s">Message</a> -
         </div>
     );
 }

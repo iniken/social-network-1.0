c55a9399420d32f757e7c84a67a56f2bbf5789fd
iniken <kiparis994@gmail.com>
2020-08-07 21:16:55 +0700

    9) add import export <Header/> and <Technologies/>

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div>
+            <a href="#S">Home</a>
+            <a href="#s">News Feed</a>
+            <a href="#s">Message</a>
+        </div>
+    );
+}
+
+export default Header;

bf25a1535b337d22c638f61f3540709edcbc8b73
iniken <kiparis994@gmail.com>
2020-08-08 21:21:09 +0700

    11) Layout of grids

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import './App.css';
-import Header from "./Header";
-import Technologies from "./Technologies";
-import Footer from "./Footer";
+
 
 const App = () => {
     return (
-        <div>
-            <Header/>
-            <Technologies/>
-            <Footer />
+        <div className="app-wrapper">
+            <header className="header">
+                <img src="https://pinfeeds.net/wp-content/uploads/2018/01/bower_logo_1-1024x900.png"/>
+            </header>
+            <nav className="nav">
+                <div><a>Profile</a></div>
+                <div><a>Messages</a></div>
+                <div><a>News</a></div>
+                <div><a>Music</a></div>
+                <div><a>Settings</a></div>
+            </nav>
+
+            <div className="content">
+                <img
+                    src="https://www.tropicbreeze.co.uk/blog/wp-content/uploads/2017/08/Nisbet-Plantation-1200-500.jpg"/>
+                <div>Ava + description</div>
+                <div>
+                    My post
+                    <div>New post</div>
+                    <div>
+                        <div>post 1</div>
+                        <div>post 2</div>
+                        <div>post 3</div>
+                        <div>post 4</div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }

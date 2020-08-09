fb16d9ba1096ec2e09cd83e8c8198e6c0eb9cb72
iniken <kiparis994@gmail.com>
2020-08-09 09:50:16 +0700

    14) Add  css-modules

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
+import s from "./Content.module.css"
 
 const Content = () => {
     return (
-        <div className="content">
+        <div className={s.content}>
             <img
                 src="https://www.tropicbreeze.co.uk/blog/wp-content/uploads/2017/08/Nisbet-Plantation-1200-500.jpg"/>
             <div>Ava + description</div>
             <div>
                 My post
                 <div>New post</div>
-                <div>
-                    <div>post 1</div>
-                    <div>post 2</div>
+                <div className={s.posts}>
+                    <div className={s.item}>post 1</div>
+                    <div >post 2</div>
                     <div>post 3</div>
-                    <div>post 4</div>
+                    <div className={s.item}>post 4</div>
                 </div>
             </div>
         </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from "./Header.module.css"
 
 const Header = () => {
     return (
-        <div className="header">
+        <div className={s.header}>
             <img src="https://pinfeeds.net/wp-content/uploads/2018/01/bower_logo_1-1024x900.png"/>
         </div>
     )

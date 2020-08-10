f03bbbe0af3fd913e7dbef29f0a52471cd1ed894
iniken <kiparis994@gmail.com>
2020-08-10 17:48:15 +0700

    21) Layout dialogs

--- src/components/Dialog/Dialog.jsx
+++ src/components/Dialog/Dialog.jsx
@@ -4,7 +4,21 @@ import s from "./Dialog.module.css"
 const Dialog = () => {
     return (
         <div>
-            Dialog
+            <div className={s.dialogs}>
+                <div className={s.dialogsItem}>
+                        <div className={s.dialog + " " + s.active}>Dimych</div>
+                        <div className={s.dialog}>Andrey</div>
+                        <div className={s.dialog}>Sveta</div>
+                        <div className={s.dialog}>Sasha</div>
+                        <div className={s.dialog}>Viktor</div>
+                        <div className={s.dialog}>Valera</div>
+                </div>
+                <div className={s.messages}>
+                    <div className={s.message}>Hi</div>
+                    <div className={s.message}>How is your it-kamasutra?</div>
+                    <div className={s.message}>How are you?</div>
+                </div>
+            </div>
         </div>
     )
 }

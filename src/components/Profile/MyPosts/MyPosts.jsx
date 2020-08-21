acacf32fd00caf98ca70324db36f94a51aa15f58
iniken <kiparis994@gmail.com>
2020-08-21 16:14:30 +0700

    31) Add onClick, ref, VirtualDOM

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -7,16 +7,21 @@ const MyPosts = (props) => {
     let postElements = props.postsData.map( postMap =>
         <Post mes={postMap.message} like={postMap.likesCount}/>
     )
+    let newPostElement= React.createRef();
 
+    let AddPost = () => {
+        let text = newPostElement.current.value
+     alert(text)
+    }
     return (
         <div className={s.myPostsBlock}>
             <h3>My post</h3>
             <div>
                 <div>
-                    <textarea></textarea>
+                    <textarea ref={newPostElement}></textarea>
                 </div>
                 <div>
-                    <button>Add post</button>
+                    <button onClick={AddPost}>Add post</button>
                 </div>
             </div>
             <div className={s.posts}>
